7c2774f7e6c2470d3de3a73fe9dc930515c5d986
Cristhian Morales <sistematizaref.programador4@gmail.com>
2017-05-11 16:17:09 -0500

    Info modelos logger

--- app/Models/Actividades.php
+++ app/Models/Actividades.php
@@ -77,6 +77,7 @@ class Actividades extends Model
      */
     public function add_task_user($data,$cactividad=null)
     {
+        $user = Auth::user();
         $actividad = $this;
         if ( $cactividad ){
             $actividad = Tareas::where('cactividad', $cactividad)->first();
@@ -86,9 +87,11 @@ class Actividades extends Model
         if( AsignacionTareas::where(array('cactividad' => $actividad->cactividad ,'ctarea'=>$data["ctarea"],'user'=>$data["user"]))->exists() ){
             AsignacionTareas::where(array('cactividad' => $actividad->cactividad ,'ctarea'=>$data["ctarea"],'user'=>$data["user"]))->update(['ctirelacion'=>$data["ctirelacion"]]);
             $obj = AsignacionTareas::where(array('cactividad' => $actividad->cactividad ,'ctarea'=>$data["ctarea"],'user'=>$data["user"]))->first();
+            Log::info('responsable editado en tarea,',['user (edito)' => $user->id, '' => $data['cactividad'] ,'tarea' => $data['ctarea'] , 'user (responsable)' =>$data["user"]]);
             $data = array("message"=>"Se edito la <b>Responsabilidad</b> del Usuario.");
         }else{
             $obj = AsignacionTareas::create($dataCreataion);
+            Log::info('responsable creado en tarea,',['user (edito)' => $user->id, 'actividad' => $obj->cactividad ,'tarea' => $obj->ctarea , 'user (respo)' =>$obj->user]);
             $data = array("message"=>"El usuario se agregro exitosamente");
         }
         return array("obj"=>$obj,"data"=>$data);
